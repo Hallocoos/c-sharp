d4659adda8403d70bcf5f299aff7809984287232
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2021-05-26 10:38:49 +0200

    first commit

---
+++ News/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            Console.WriteLine("Average: " + (result / grades.Count));
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello!");
+            }
+        }
+    }
+}
