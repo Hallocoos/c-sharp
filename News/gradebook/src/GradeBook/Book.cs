6fb0d88d52445328e3d847274262ebc13cb6f3e5
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2021-05-30 13:16:39 +0200

    module complete

--- News/gradebook/src/GradeBook/Book.cs
+++ News/gradebook/src/GradeBook/Book.cs
@@ -3,18 +3,104 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+    public class Book
     {
         private List<double> grades;
+        private string name;
+        public const string CATEGORY = "Science";
 
-        public Book()
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public Book(string name)
         {
             this.grades = new List<double>();
+            this.Name = name;
         }
 
         public void AddGrade(double grade)
         {
-            this.grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                this.grades.Add(grade);
+
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public event GradeAddedDelegate GradeAdded;
+
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.High = double.MinValue;
+            result.Average = 0.0;
+            result.Low = double.MaxValue;
+
+            for (var index = 0; index < grades.Count; index += 1)
+            {
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+            }
+            result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90:
+                    result.letter = 'A';
+                    break;
+                case var d when d >= 80:
+                    result.letter = 'B';
+                    break;
+                case var d when d >= 70:
+                    result.letter = 'C';
+                    break;
+                case var d when d >= 60:
+                    result.letter = 'D';
+                    break;
+                case var d when d >= 50:
+                    result.letter = 'F';
+                    break;
+            }
+            return (result);
         }
     }
 }
--- News/gradebook/src/GradeBook/Program.cs
+++ News/gradebook/src/GradeBook/Program.cs
@@ -7,20 +7,57 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Grades");
+            book.GradeAdded += OnGradeAdded;
+            book.GradeAdded += OnGradeAdded;
+            book.GradeAdded -= OnGradeAdded;
+            book.GradeAdded += OnGradeAdded;
+
+            while (true)
+            {
+                Console.WriteLine($"Enter a grade or 'q' to quit: ");
+                var input = Console.ReadLine();
+
+                if (input == "q" || input == "")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
             book.AddGrade(89.1);
             book.AddGrade(90.5);
+            book.AddGrade(77.5);
 
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
+            var stats = book.GetStatistics();
 
-            var result = 0.0;
-            foreach (double number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"Average: {result}");
+            Console.WriteLine(Book.CATEGORY);
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is: {stats.Low}");
+            Console.WriteLine($"Average: {stats.Average}");
+            Console.WriteLine($"The highest grade is: {stats.High}");
+            Console.WriteLine($"The letter grade is {stats.letter}");
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
         }
     }
 }
